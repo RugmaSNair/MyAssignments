b8762cf007c3076eaa5c3158857c7efcfa8ae9d9
RugmaSNair <141847090+RugmaSNair@users.noreply.github.com>
2023-10-19 09:38:56 +0530

    Add files via upload

---
+++ week4/day1/BaseClass.java
@@ -0,0 +1,35 @@
+package week4.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+	
+	public ChromeDriver driver;
+	
+    @BeforeMethod
+	public void preCondition () {
+    	
+		 driver = new ChromeDriver(); //make this as global declaration so that all methods can access
+		
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+	}
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+	}
+}
